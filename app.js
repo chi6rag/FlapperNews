b68a44268ac2d4bc65b30634ffbbea4561dc71a5
Chirag Aggarwal <chi6rag@gmail.com>
2015-05-25 16:39:56 +0530

    Shift posts to factory

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
 flapperNews = angular.module('flapperNews', [])
 .controller('MainCtrl', [
-	'$scope',
-	function($scope){
-		$scope.posts = [
-			{ title: 'First Post', upvotes: 100, link: 'https://www.example.com' },
-			{ title: 'Second Post', upvotes: 200, link: 'https://www.example.com' },
-			{ title: 'Third Post', upvotes: 300, link: 'https://www.example.com' },
-			{ title: 'Fourth Post', upvotes: 400, link: 'https://www.example.com'  },
-			{ title: 'Fifth Post', upvotes: 500, link: 'https://www.example.com'  }
-		];
+	'$scope', 'posts',
+	function($scope, posts){
+		$scope.posts = posts.posts;
 		$scope.addPost = function(){
 			console.log($scope.link);
 			if($scope.title==undefined || $scope.title.match(/^\s+$/)){ return; }
@@ -23,4 +17,16 @@ flapperNews = angular.module('flapperNews', [])
 		$scope.incrementUpvotes = function(post){
 			post.upvotes += 1
 		};
+}]);
+
+flapperNews.factory('posts', [function(){
+	var output = {};
+	output.posts = [
+		{ title: 'First Post', upvotes: 100, link: 'https://www.example.com' },
+		{ title: 'Second Post', upvotes: 200, link: 'https://www.example.com' },
+		{ title: 'Third Post', upvotes: 300, link: 'https://www.example.com' },
+		{ title: 'Fourth Post', upvotes: 400, link: 'https://www.example.com'  },
+		{ title: 'Fifth Post', upvotes: 500, link: 'https://www.example.com'  }
+  ];
+  return output;
 }]);
