4b544531e861449d05bf6dadfc6f897c52fead1a
Chirag Aggarwal <chi6rag@gmail.com>
2015-05-25 15:25:50 +0530

    Improve style

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ flapperNews = angular.module('flapperNews', [])
 .controller('MainCtrl', [
 	'$scope',
 	function($scope){
-		$scope.name = "Chirag";
 		$scope.posts = [
-			{ title: 'one lorem', upvotes: 100 },
-			{ title: 'two lorem', upvotes: 200 },
-			{ title: 'three lorem', upvotes: 300 },
-			{ title: 'four lorem', upvotes: 400  },
-			{ title: 'five lorem', upvotes: 500  }
+			{ title: 'First Post', upvotes: 100, link: 'https://www.example.com' },
+			{ title: 'Second Post', upvotes: 200, link: 'https://www.example.com' },
+			{ title: 'Third Post', upvotes: 300, link: 'https://www.example.com' },
+			{ title: 'Fourth Post', upvotes: 400, link: 'https://www.example.com'  },
+			{ title: 'Fifth Post', upvotes: 500, link: 'https://www.example.com'  }
 		];
-		$scope.addPost = function(title){
+		$scope.addPost = function(){
+			console.log($scope.link);
 			if($scope.title==undefined || $scope.title.match(/^\s+$/)){ return; }
 			$scope.posts.push({
 				title: $scope.title,
