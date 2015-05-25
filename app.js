74264ccce8bb064fcb5041a3479b3e464be1ae54
Chirag Aggarwal <chi6rag@gmail.com>
2015-05-25 12:21:25 +0530

    Implement the add post functionality; add empty string validation

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ flapperNews = angular.module('flapperNews', [])
 .controller('MainCtrl', [
 	'$scope',
 	function($scope){
-		$scope.name = "Chirag"
+		$scope.name = "Chirag";
 		$scope.posts = [
-			{ title: 'one lorem', upvotes: 500 },
-			{ title: 'two lorem', upvotes: 400 },
+			{ title: 'one lorem', upvotes: 100 },
+			{ title: 'two lorem', upvotes: 200 },
 			{ title: 'three lorem', upvotes: 300 },
-			{ title: 'four lorem', upvotes: 200  },
-			{ title: 'five lorem', upvotes: 100  }
-		]
+			{ title: 'four lorem', upvotes: 400  },
+			{ title: 'five lorem', upvotes: 500  }
+		];
+		$scope.addPost = function(title){
+			if($scope.title==undefined || $scope.title.match(/^\s+$/)){return;}
+			$scope.posts.push({title: title, upvotes: 0});
+			$scope.title = '';
+		};
 }]);
