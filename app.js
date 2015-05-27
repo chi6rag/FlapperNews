9e429d349b0ebd282c55fef4ffa996196db4ccc4
Chirag Aggarwal <chi6rag@gmail.com>
2015-05-27 10:31:06 +0530

    - Update routes - Correct broken comments functionality - Correct partials now showing bug - add link to go to post's comments from home page

--- app.js
+++ app.js
@@ -1,38 +1,42 @@
 var flapperNews = angular.module('flapperNews', ['ui.router']);
 
 flapperNews.config([
-	'$stateProvider',
-	'$urlRouterProvider',
-	function($stateProvider, $urlRouterProvider){
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider){
+	$stateProvider
+	  
+	  .state('home', {
+	  	url: '/home',
+	  	templateUrl: '/home.html',
+	  	controller: 'MainCtrl'
+	  })
 
-		$stateProvider
-		  .state('home', {
-		  	url: '/home',
-		  	templateUrl: '/home.html',
-		  	controller: 'MainCtrl'
-		  })
+	  .state('posts', {
+	  	url: '/posts/{id}',
+	  	templateUrl: '/posts.html',
+	  	controller: 'PostsCtrl'
+	  })
 
-		  .state('posts', {
-		  	url: '/posts/{id}',
-		  	templateUrl: 'posts.html',
-		  	controller: 'PostsCtrl'
-		  });
+	  $urlRouterProvider.otherwise('/home');
 
-		$urlRouterProvider.otherwise('/home');
-	}
-]);
+}]);
 
 flapperNews.controller('MainCtrl', [
 	'$scope', 'posts',
 	function($scope, posts){
 		$scope.posts = posts.posts;
 		$scope.addPost = function(){
-			console.log($scope.link);
 			if($scope.title==undefined || $scope.title.match(/^\s+$/)){ return; }
 			$scope.posts.push({
 				title: $scope.title,
 				link: $scope.link,
-				upvotes: 0
+				upvotes: 0,
+				comments: [
+					{author: 'chi6rag', body: 'lorem ipsum dolor', upvotes: 0},
+					{author: 'chi6rag', body: 'lorem ipsum dolor', upvotes: 0},
+					{author: 'chi6rag', body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', upvotes: 0}
+				]
 			});
 			$scope.title = '';
 			$scope.link = '';
@@ -43,23 +47,9 @@ flapperNews.controller('MainCtrl', [
 }]);
 
 flapperNews.controller('PostsCtrl', [
-	'$scope', '$stateParams', 'posts',
-	function($scope, $stateParams, posts){
-		$scope.posts = posts.posts;
-		var postToPush = {
-			title: 'Pushed Post',
-			upvotes: 0,
-			link: 'http://chi6rag.example.com',
-			comments: [
-				{ author: 'Chirag', body: 'lorem ipsum dolor', upvotes: 0 },
-				{ author: 'Ishan', body: 'lorem ipsum dolor', upvotes: 0 },
-				{ author: 'Arnav', body: 'lorem ipsum dolor', upvotes: 0 },
-				{ author: 'Saurav', body: 'lorem ipsum dolor', upvotes: 0 },
-				{ author: 'Laavanya', body: 'lorem ipsum dolor', upvotes: 0 }
-			]
-		}
-		$scope.posts.push(postToPush);
-		$scope.selected_post = posts.posts[$stateParams.id]
+	'$scope', 'posts', '$stateParams',
+	function($scope, posts, $stateParams){
+		$scope.post = posts.posts[$stateParams.id];
 	}
 ]);
 
